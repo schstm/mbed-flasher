67c42addf06e2cb964cc5eb579220202c85783d5
yifeizuo01 <yifei.zuo@arm.com>
2017-08-13 21:04:57 +0300

    fix `flashers` module pylint warnings

--- mbed_flasher/flashers/FlasherMbed.py
+++ mbed_flasher/flashers/FlasherMbed.py
@@ -15,20 +15,23 @@ limitations under the License.
 """
 
 import logging
-import six
 from os.path import join, abspath, isfile
 import os
 import platform
 from time import sleep, time
 from threading import Thread
-from mbed_flasher.flashers.enhancedserial import EnhancedSerial
-from serial.serialutil import SerialException
 import hashlib
-import mbed_lstools
 from subprocess import PIPE, Popen
+from serial.serialutil import SerialException
+import six
+from mbed_flasher.flashers.enhancedserial import EnhancedSerial
+import mbed_lstools
 
 
 class FlasherMbed(object):
+    """
+    Implementation class of mbed-flasher flash operation
+    """
     name = "mbed"
     FLASHING_VERIFICATION_TIMEOUT = 100
 
@@ -37,23 +40,34 @@ class FlasherMbed(object):
 
     @staticmethod
     def get_supported_targets():
-        """Load target mapping information
+        """
+        Load target mapping information
         """
         mbeds = mbed_lstools.create()
         return sorted(set(mbeds.manufacture_ids.values()))
 
     @staticmethod
     def get_available_devices():
+        """
+        Get available devices
+        """
         mbeds = mbed_lstools.create()
         return mbeds.list_mbeds()
-        
+
     def reset_board(self, serial_port):
+        """
+        Reset board
+        """
         try:
             port = EnhancedSerial(serial_port)
-        except SerialException as e:
+        except SerialException as err:
             self.logger.info("reset could not be sent")
-            self.logger.error(e)
-            if e.message.find('could not open port') != -1:
+            self.logger.error(err)
+            # SerialException.message is type "string"
+            # pylint: disable=no-member
+            if err.message.find('could not open port') != -1:
+                # python 3 compatibility
+                # pylint: disable=superfluous-parens
                 print('Reset could not be given. Close your Serial connection to device.')
             return -6
         port.baudrate = 115200
@@ -71,8 +85,14 @@ class FlasherMbed(object):
             else:
                 self.logger.info("reset failed")
         port.close()
-    
+
+
+    # no need to use self
+    # pylint: disable=no-self-use
     def auxiliary_drive_check(self, drive):
+        """
+        Check if auxiliary drive exists
+        """
         out = os.listdir(drive[0])
         for item in out:
             if item.find('.HTM') != -1:
@@ -81,13 +101,20 @@ class FlasherMbed(object):
             return False
         if drive[1] not in out:
             return True
-       
+
     def runner(self, drive):
+        """
+        Runner
+        """
         start_time = time()
         while True:
             sleep(2)
             if platform.system() == 'Windows':
-                proc = Popen(["dir", drive[0]], stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+                proc = Popen(["dir", drive[0]],
+                             stdin=PIPE,
+                             stdout=PIPE,
+                             stderr=PIPE,
+                             shell=True)
             else:
                 proc = Popen(["ls", drive[0]], stdin=PIPE, stdout=PIPE, stderr=PIPE)
             out = proc.stdout.read()
@@ -100,15 +127,21 @@ class FlasherMbed(object):
                     if self.auxiliary_drive_check(drive):
                         break
             if time() - start_time > self.FLASHING_VERIFICATION_TIMEOUT:
-                self.logger.debug("re-mount check timed out for %s" % drive[0])
+                self.logger.debug("re-mount check timed out for %s", drive[0])
                 break
-                
+
+    # pylint: disable=too-many-branches
     def check_points_unchanged(self, target):
+        """
+        Check if points are unchanged
+        """
         new_target = {}
         if platform.system() == 'Windows':
             mbeds = mbed_lstools.create()
             if target['serial_port'] != mbeds.get_mbed_com_port(target['target_id']):
                 new_target['serial_port'] = mbeds.get_mbed_com_port(target['target_id'])
+        # confusing pylint warning, no nested blocks in next line
+        # # pylint: disable=too-many-nested-blocks
         elif platform.system() == 'Darwin':
             pass
         else:
@@ -118,8 +151,9 @@ class FlasherMbed(object):
                         if 'serial_port' not in new_target:
                             new_target['serial_port'] = '/dev/' + line.split('/')[-1]
                         else:
-                            self.logger.error("target_id %s has more than 1 "
-                                              "serial port in the system" % target['target_id'])
+                            self.logger.error('target_id %s has more than 1 '
+                                              'serial port in the system',
+                                              target['target_id'])
                             return -10
             dev_points = []
             for dev_line in os.popen('ls -oA /dev/disk/by-id/').read().splitlines():
@@ -128,45 +162,50 @@ class FlasherMbed(object):
                         new_target['dev_point'] = '/dev/' + dev_line.split('/')[-1]
                     else:
                         self.logger.error("target_id %s has more than 1 "
-                                          "device point in the system" % target['target_id'])
+                                          "device point in the system",
+                                          target['target_id'])
                         return -11
             if dev_points:
-                for i in range(10):
+                for _ in range(10):
                     for_break = False
                     mounts = os.popen('mount |grep vfat').read().splitlines()
                     for mount in mounts:
                         if mount.find(new_target['dev_point']) != -1:
-                            if target['mount_point'] == mount.split('on')[1].split('type')[0].strip():
+                            if target['mount_point'] == \
+                                    mount.split('on')[1].split('type')[0].strip():
                                 for_break = True
                                 break
                             else:
-                                new_target['mount_point'] = mount.split('on')[1].split('type')[0].strip()
+                                new_target['mount_point'] = \
+                                    mount.split('on')[1].split('type')[0].strip()
                                 for_break = True
                                 break
                         sleep(1)
                     if for_break:
                         break
                 else:
-                    self.logger.error("vfat mount point for %s did not re-appear in the system in 10 seconds" % 
-                                      target['target_id'])
+                    self.logger.error("vfat mount point for %s did not re-appear in "
+                                      "the system in 10 seconds", target['target_id'])
                     return -12
-        
+
         if new_target:
             if 'serial_port' in new_target:
-                self.logger.debug("serial port %s has changed to %s" %
-                                  (target['serial_port'], new_target['serial_port']))
+                self.logger.debug("serial port %s has changed to %s",
+                                  target['serial_port'], new_target['serial_port'])
             else:
-                self.logger.debug("serial port %s has not changed" % target['serial_port'])
+                self.logger.debug("serial port %s has not changed",
+                                  target['serial_port'])
             if 'mount_point' in new_target:
-                self.logger.debug("mount point %s has changed to %s" %
-                                  (target['mount_point'], new_target['mount_point']))
+                self.logger.debug("mount point %s has changed to %s",
+                                  target['mount_point'], new_target['mount_point'])
             else:
-                self.logger.debug("mount point %s has not changed" % target['mount_point'])
+                self.logger.debug("mount point %s has not changed",
+                                  target['mount_point'])
             new_target['target_id'] = target['target_id']
             return new_target
         else:
             return target
-            
+
     def flash(self, source, target, method, no_reset):
         """copy file to the destination
         :param source: binary to be flashed
@@ -179,33 +218,36 @@ class FlasherMbed(object):
             try:
                 from pyOCD.board import MbedBoard
             except ImportError:
+                # python 3 compatibility
+                # pylint: disable=superfluous-parens
                 print('pyOCD missing, install\n')
                 return -8
         if method == 'edbg':
             self.logger.debug("edbg is not supported for Mbed devices")
             return -13
-        
+
         mount_point = os.path.abspath(target['mount_point'])
-        (head, tail) = os.path.split(os.path.abspath(source))
+        (_, tail) = os.path.split(os.path.abspath(source))
         destination = abspath(join(mount_point, tail))
-        
+
         if isinstance(source, six.string_types):
             if method == 'pyocd':
                 try:
                     with MbedBoard.chooseBoard(board_id=target["target_id"]) as board:
                         ocd_target = board.target
                         ocd_flash = board.flash
-                        self.logger.debug("resetting device: %s" % target["target_id"])
+                        self.logger.debug("resetting device: %s", target["target_id"])
                         sleep(0.5)  # small sleep for lesser HW ie raspberry
                         ocd_target.reset()
-                        self.logger.debug("flashing device: %s" % target["target_id"])
+                        self.logger.debug("flashing device: %s", target["target_id"])
                         ocd_flash.flashBinary(source)
-                        self.logger.debug("resetting device: %s" % target["target_id"])
+                        self.logger.debug("resetting device: %s", target["target_id"])
                         sleep(0.5)  # small sleep for lesser HW ie raspberry
                         ocd_target.reset()
                     return 0
-                except AttributeError as e:
-                    self.logger.error("Flashing failed: %s. tid=%s" % (e, target["target_id"]))
+                except AttributeError as err:
+                    self.logger.error("Flashing failed: %s. tid=%s",
+                                      err, target["target_id"])
                     return -4
             else:
                 try:
@@ -213,48 +255,61 @@ class FlasherMbed(object):
                         self.reset_board(target['serial_port'])
                         sleep(0.1)
                     if platform.system() == 'Windows':
-                        with open(source, 'rb') as f:
-                            aux_source = f.read()
-                            self.logger.debug("SHA1: %s" % hashlib.sha1(aux_source).hexdigest())
-                        self.logger.debug("copying file: %s to %s" % (source, destination))
+                        with open(source, 'rb') as source_file:
+                            aux_source = source_file.read()
+                            self.logger.debug("SHA1: %s",
+                                              hashlib.sha1(aux_source).hexdigest())
+                        self.logger.debug("copying file: %s to %s",
+                                          source, destination)
                         os.system("copy %s %s" % (os.path.abspath(source), destination))
                     else:
                         self.logger.debug('read source file')
-                        with open(source, 'rb') as f:
-                            aux_source = f.read()
+                        with open(source, 'rb') as source_file:
+                            aux_source = source_file.read()
                         if not aux_source:
                             self.logger.error("File couldn't be read")
                             return -7
-                        self.logger.debug("SHA1: %s" % hashlib.sha1(aux_source).hexdigest())
-                        self.logger.debug("writing binary: %s (size=%i bytes)", destination, len(aux_source))
+                        self.logger.debug("SHA1: %s",
+                                          hashlib.sha1(aux_source).hexdigest())
+                        self.logger.debug("writing binary: %s (size=%i bytes)",
+                                          destination,
+                                          len(aux_source))
 
                         def get_me_file():
+                            """
+                            Get file
+                            """
                             if platform.system() == 'Darwin':
-                                return os.open(destination, os.O_CREAT | os.O_TRUNC | os.O_RDWR | os.O_SYNC)
+                                return os.open(
+                                    destination,
+                                    os.O_CREAT | os.O_TRUNC | os.O_RDWR | os.O_SYNC)
                             else:
-                                return os.open(destination, os.O_CREAT | os.O_DIRECT | os.O_TRUNC | os.O_RDWR)
+                                return os.open(
+                                    destination,
+                                    os.O_CREAT | os.O_DIRECT | os.O_TRUNC | os.O_RDWR)
                         new_file = get_me_file()
                         os.write(new_file, aux_source)
                         os.close(new_file)
                     self.logger.debug("copy finished")
                     sleep(4)
-                    
+
                     new_target = self.check_points_unchanged(target)
-                    
+
                     if isinstance(new_target, int):
                         return new_target
                     else:
-                        t = Thread(target=self.runner, args=([target['mount_point'], tail],))
-                        t.start()
-                        while t.is_alive():
-                            t.join(2.5)
+                        thread = Thread(target=self.runner,
+                                        args=([target['mount_point'], tail],))
+                        thread.start()
+                        while thread.is_alive():
+                            thread.join(2.5)
                         if not no_reset:
                             if 'serial_port' in new_target:
                                 self.reset_board(new_target['serial_port'])
                             else:
                                 self.reset_board(target['serial_port'])
                             sleep(0.4)
-                            
+
                         # verify flashing went as planned
                         self.logger.debug("verifying flash")
                         if 'mount_point' in new_target:
@@ -264,23 +319,25 @@ class FlasherMbed(object):
                         if isfile(join(mount, 'FAIL.TXT')):
                             with open(join(mount, 'FAIL.TXT'), 'r') as fault:
                                 fault = fault.read().strip()
-                            self.logger.error("Flashing failed: %s. tid=%s" % (fault, target["target_id"]))
+                            self.logger.error("Flashing failed: %s. tid=%s",
+                                              fault, target["target_id"])
                             return -4
                         if isfile(join(mount, 'ASSERT.TXT')):
                             with open(join(mount, 'ASSERT.TXT'), 'r') as fault:
                                 fault = fault.read().strip()
-                            self.logger.error("Flashing failed: %s. tid=%s" % (fault, target))
+                            self.logger.error("Flashing failed: %s. tid=%s",
+                                              fault, target)
                             return -4
                         if isfile(join(mount, tail)):
-                            self.logger.error("Flashing failed: File still present in mount point. tid info: %s" %
-                                              target)
+                            self.logger.error("Flashing failed: File still present "
+                                              "in mount point. tid info: %s", target)
                             return -15
                         self.logger.debug("ready")
                         return 0
                 except IOError as err:
                     self.logger.error(err)
                     raise err
-                except OSError as e:
+                except OSError as err:
                     self.logger.error("Write failed due to OSError")
-                    self.logger.error(e)
+                    self.logger.error(err)
                     return -14
