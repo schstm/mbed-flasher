2257e7ecc54e713ca0344320cb089e661d1b0d2a
yifeizuo01 <yifei.zuo@arm.com>
2017-08-15 13:57:28 +0300

    change usage of safe_send_break()

--- mbed_flasher/flashers/FlasherMbed.py
+++ mbed_flasher/flashers/FlasherMbed.py
@@ -79,7 +79,7 @@ class FlasherMbed(object):
 
         if port:
             self.logger.info("sendBreak to device to reboot")
-            result = port.safe_sendBreak()
+            result = port.safe_send_break()
             if result:
                 self.logger.info("reset completed")
             else:
